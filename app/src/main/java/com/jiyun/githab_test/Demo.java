142b9f48b3800209698c28aee9520666c40b3715
1397457773 <1397457773@qq.com>
2017-08-14 22:26:28 +0800

    第二次提交 框架内容填充，席间newMenthod方法
    时间：2017年8月14日22:26:18
    程序员：李俊岐

---
+++ app/src/main/java/com/jiyun/githab_test/Demo.java
@@ -0,0 +1,15 @@
+package com.jiyun.githab_test;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Lenovo on 2017/8/14.
+ */
+
+public class Demo {
+    private void newMethod(){
+        Log.e("TAG", "newMethod: " );
+    }
+}
